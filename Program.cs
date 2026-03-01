89fdb61a46d856cc99a993fc075f27720a611796
Bishesh <bisheshkhatiwada15@gmail.com>
2026-03-01 21:11:41 +0545

    made feature.cs

--- Program.cs
+++ Program.cs
@@ -1,79 +1,15 @@
 ﻿using System;
 
-// ABSTRACTION
-abstract class Animal
+namespace FullstackDemo
 {
-    public string Name { get; set; }
-
-    // Abstract method
-    public abstract void MakeSound();
-
-    // Normal method
-    public void Eat()
+    class Program
     {
-        Console.WriteLine(Name + " is eating.");
-    }
-}
-
-// INHERITANCE
-class Dog : Animal
-{
-    // POLYMORPHISM (Method Overriding)
-    public override void MakeSound()
-    {
-        Console.WriteLine(Name + " says: Woof Woof!");
-    }
-}
-
-class Cat : Animal
-{
-    public override void MakeSound()
-    {
-        Console.WriteLine(Name + " says: Meow!");
-    }
-}
-
-// ENCAPSULATION
-class Person
-{
-    private int age;
-
-    public int Age
-    {
-        get { return age; }
-        set
+        static void Main(string[] args)
         {
-            if (value > 0)
-                age = value;
+            Features f = new Features();
+            f.Run();
+
+            Console.ReadLine();
         }
-    }
-
-    public void ShowAge()
-    {
-        Console.WriteLine("Age: " + age);
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Person p = new Person();
-        p.Age = 19;
-        p.ShowAge();
-
-
-        Animal dog = new Dog();
-        dog.Name = "Tommy";
-        dog.MakeSound();
-        dog.Eat();
-
-
-        Animal cat = new Cat();
-        cat.Name = "Kitty";
-        cat.MakeSound();
-        cat.Eat();
-
-        Console.ReadLine();
     }
 }
---
+++ features.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FullstackDemo
+{
+    abstract class Animal
+    {
+        public string Name { get; set; }
+
+        public abstract void MakeSound();
+
+        public void Eat()
+        {
+            Console.WriteLine(Name + " is eating.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine(Name + " says: Woof Woof!");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine(Name + " says: Meow!");
+        }
+    }
+
+    class Person
+    {
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                    age = value;
+            }
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine("Age: " + age);
+        }
+    }
+
+    internal class Features
+    {
+        public void Run()
+        {
+            Person p = new Person();
+            p.Age = 19;
+            p.ShowAge();
+
+            Animal dog = new Dog();
+            dog.Name = "Tommy";
+            dog.MakeSound();
+            dog.Eat();
+
+            Animal cat = new Cat();
+            cat.Name = "Kitty";
+            cat.MakeSound();
+            cat.Eat();
+        }
+    }
+}
